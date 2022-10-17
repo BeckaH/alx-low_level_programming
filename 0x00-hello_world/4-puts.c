8549e42694dd91df4f7bba0e0b6e92277d67943c
BeckaH <bekahabtamu@gmail.com>
2022-10-17 14:24:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - write 'Programming is like a puzzle'
+ * main - write 'Programming is like building a puzzle'
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
-}	
+}
