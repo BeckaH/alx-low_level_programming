9796ae712609cd543d6b8a369f689898d40fd3f2
BeckaH <bekahabtamu@gmail.com>
2022-10-19 14:51:33 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value function
+ * for input
+ *
+ * @a: input parameter
+ * Return: abs value
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
+}
