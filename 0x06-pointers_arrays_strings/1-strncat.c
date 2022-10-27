6f4da6b8d05ff71d062bff030b12ceb2818c0f3c
BeckaH <bekahabtamu@gmail.com>
2022-10-27 14:46:14 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings
+ * @dest: input parameter
+ * @src: second input parameter
+ * @n: third input parameter
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[j++] = src[i];
+	return (dest)
+}
