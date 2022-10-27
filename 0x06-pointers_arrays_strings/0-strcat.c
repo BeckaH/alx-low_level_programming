3c815a86a06dd17a9f1b1a222883a1ca0d5703c5
BeckaH <bekahabtamu@gmail.com>
2022-10-27 14:21:58 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - comcatenates wto strings
+ * @dest: input parameter
+ * @src: second input parameter
+ * Return:pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+	return (dest);
+}
+
